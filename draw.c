e25af72a574dcb6e15f99b39fcc4516a3b24c3a2
Martin Uecker <muecker@gwdg.de>
2015-10-25 19:14:46 +0100

    add BSD license

--- draw.c
+++ draw.c
@@ -1,4 +1,6 @@
-/* All rights reserved.
+/* Copyright 2015. Martin Uecker.
+ * All rights reserved. Use of this source code is governed by
+ * a BSD-style license which can be found in the LICENSE file.
  *
  * Author:
  *	2015 Martin Uecker <martin.uecker@med.uni-goettingen.de>
